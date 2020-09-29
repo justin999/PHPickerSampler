0aae39cc5c9889726614953dd55829f0944344a6
Justin Sato <koichi.sato.aow@gmail.com>
2020-09-30 06:39:30 +0900

    initial

---
+++ PHPickerSampler/PHPickerSamplerApp.swift
@@ -0,0 +1,18 @@
+//
+//  PHPickerSamplerApp.swift
+//  PHPickerSampler
+//
+//  Created by Justin Sato on 2020/09/30.
+//  
+//
+
+import SwiftUI
+
+@main
+struct PHPickerSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
